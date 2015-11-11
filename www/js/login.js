00f2ba84c3cae5b4ee3126f5048f9cc9194b6dfa
FelipeTrakiinaS <felipealexandre.ittner@gmail.com>
2015-11-11 00:44:18 -0200

    v1.7

---
+++ www/js/login.js
@@ -0,0 +1,63 @@
+function cadastro() {
+
+	var postTo = 'http://localhost/postoapp/login.php';
+
+    var nome = $('#cadastrarNome').val() ;
+    var senha = $('#cadastrarSenha').val();
+
+    console.log(nome);
+
+
+
+	$.post( postTo,({ acao : 'cadastrar',
+		              nome : nome, 
+		             senha : senha
+
+		            }), function( data ) {
+
+	});
+
+}
+
+
+function logar () {
+	
+	var postTo = 'http://localhost/postoapp/login.php';
+
+    var nome = $('#logarNome').val() ;
+    var senha = $('#logarSenha').val();
+
+    console.log(nome);
+
+
+
+	$.post( postTo,({ acao : 'logar',
+		              nome : nome, 
+		             senha : senha
+
+		            }), 
+		function( data ) {
+
+			alert(data);
+
+			var ver =  JSON.parse(data);
+
+			if (ver === 'ok') {
+
+				alert('logado');
+
+				//salvar no cache
+				//direciona para pagina
+				window.location.replace("C:/Users/Acer%20Aspire%20V5/Documents/GitHub/postoapp/www/index.html");
+
+			}else{
+				console.log('login ou senha errado');
+			}
+
+
+
+		}
+	);
+
+
+}
